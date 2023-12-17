d6a28b4018857615e2749d8a96ee3cf52fd3999d
Decrypt <louis.vicomte@protonmail.com>
2023-12-17 10:51:49 +0100

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,21 +2,28 @@ import gpt3 from '../commands/gpt3.js';
 import gpt4 from '../commands/gpt4.js';
 import gptVision from '../commands/gpt-vision.js';
 
+const commandHandlers = {
+  'gpt4': gpt4,
+  'gpt3': gpt3,
+  'gpt-vision': gptVision
+};
+
 async function interactionCreate(interaction, client) {
   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
-
-  switch (commandName) {
-    case 'gpt4':
-      await gpt4(interaction, client);
-      break;
-    case 'gpt3':
-      await gpt3(interaction, client);
-      break;
-    case 'gpt-vision':
-      await gptVision(interaction, client);
-      break;
+  const handler = commandHandlers[interaction.commandName];
+  
+  if (handler) {
+    try {
+      await handler(interaction, client);
+    } catch (error) {
+      console.error(`Error handling command ${interaction.commandName}:`, error);
+      // Send a generic error message to the user
+      await interaction.reply({ 
+        content: "Sorry, there was an error processing your command.", 
+        ephemeral: true 
+      });
+    }
   }
 }
 
