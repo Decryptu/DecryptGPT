672f4bb110377b23267a274686471a97cee6b6f4
Decrypt <louis.vicomte@protonmail.com>
2023-12-17 11:45:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Client, IntentsBitField, Partials } from 'discord.js';
 import { Configuration, OpenAIApi } from 'openai';
-import express from 'express';
+// import express from 'express';
 import 'dotenv/config.js';
 
 import interactionCreate from './events/interactionCreate.js';
@@ -9,16 +9,16 @@ import ready from './events/ready.js';
 import setBotActivity from './utils/setBotActivity.js';
 import { AI_NAME, DEFAULT_MODEL } from './config.js';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send(`${AI_NAME} is alive!`);
-});
+// app.get('/', (req, res) => {
+//   res.send(`${AI_NAME} is alive!`);
+// });
 
-let server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// let server = app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 const client = new Client({
   intents: [
@@ -68,9 +68,11 @@ async function gracefulShutdown() {
   }
 
   // Shut down the Express server
-  server.close(() => {
-    console.log('Express server closed');
-  });
+  // if (server) {
+  //   server.close(() => {
+  //     console.log('Express server closed');
+  //   });
+  // }
 }
 
 // Signal handling for graceful shutdown
