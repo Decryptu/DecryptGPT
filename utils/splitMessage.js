dd90ef65cd0d69286ce6cf68ed43560295d819e9
Decrypt <louis.vicomte@protonmail.com>
2023-12-17 10:57:58 +0100

    Update splitMessage.js

--- utils/splitMessage.js
+++ utils/splitMessage.js
@@ -1,15 +1,22 @@
+/**
+ * Splits a message into parts, each not exceeding a specified maximum length.
+ * Handles strings and objects (converted to JSON strings).
+ * 
+ * @param {string|object} content - The content to be split.
+ * @param {number} maxLength - The maximum length of each part.
+ * @returns {string[]} An array of message parts.
+ */
 function splitMessage(content, maxLength = 2000) {
-    let contentStr = typeof content === 'string' ? content : JSON.stringify(content);
-    const parts = [];
-  
-    while (contentStr.length > 0) {
-      let part = contentStr.slice(0, maxLength);
-      contentStr = contentStr.slice(maxLength);
-      parts.push(part);
-    }
-  
-    return parts;
+  let contentStr = typeof content === 'string' ? content : JSON.stringify(content);
+  const parts = [];
+
+  while (contentStr.length > 0) {
+    let part = contentStr.slice(0, maxLength);
+    contentStr = contentStr.slice(maxLength);
+    parts.push(part);
   }
-  
-  export default splitMessage;
-  
+
+  return parts;
+}
+
+export default splitMessage;
