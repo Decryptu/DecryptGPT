76311dcea88855e4062433a85767f1867603ed29
Decrypt <louis.vicomte@protonmail.com>
2023-12-17 10:53:13 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,23 +1,29 @@
 import { AI_NAME } from '../config.js';
 
 async function ready(client) {
-    console.log(`${AI_NAME} is online!`);
-    client.setBotActivity(client.currentModel);
+  console.log(`${AI_NAME} is online!`);
+  client.setBotActivity(client.currentModel);
 
-    await client.application.commands.create({
+  const commands = [
+    {
       name: 'gpt4',
       description: 'Passer au modèle GPT-4',
-    });
-
-    await client.application.commands.create({
+    },
+    {
       name: 'gpt3',
       description: 'Passer au modèle GPT-3.5',
-    });
-
-    await client.application.commands.create({
+    },
+    {
       name: 'gpt-vision',
       description: 'Passer au modèle GPT-4 Vision',
-    });
+    }
+  ];
+
+  try {
+    await client.application.commands.set(commands);
+  } catch (error) {
+    console.error("Error setting up commands:", error);
+  }
 }
 
 export default ready;
