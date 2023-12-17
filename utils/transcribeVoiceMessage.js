998bb81634df1ad788926db2ab4dc2c5569c2407
Decrypt <louis.vicomte@protonmail.com>
2023-12-17 11:01:20 +0100

    Update transcribeVoiceMessage.js

--- utils/transcribeVoiceMessage.js
+++ utils/transcribeVoiceMessage.js
@@ -1,26 +1,37 @@
 import axios from 'axios';
-import fs from 'fs';
+import fs, { promises as fsPromises } from 'fs';
 import FormData from 'form-data';
+import { randomUUID } from 'crypto';
 
 async function transcribeVoiceMessage(url, apiKey) {
-  const response = await axios.get(url, { responseType: 'arraybuffer' });
-  const data = response.data;
-  const filePath = 'temp_voice_message.ogg';
-  fs.writeFileSync(filePath, data);
+  let filePath;
 
-  const formData = new FormData();
-  formData.append('file', fs.createReadStream(filePath));
-  formData.append('model', 'whisper-1');
+  try {
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
+    const data = response.data;
+    filePath = `temp_voice_message_${randomUUID()}.ogg`;
+    await fsPromises.writeFile(filePath, data);
 
-  const transcriptionResponse = await axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
-    headers: {
-      ...formData.getHeaders(),
-      'Authorization': `Bearer ${apiKey}`
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(filePath));
+    formData.append('model', 'whisper-1');
+
+    const transcriptionResponse = await axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
+      headers: {
+        ...formData.getHeaders(),
+        'Authorization': `Bearer ${apiKey}`
+      }
+    });
+
+    return transcriptionResponse.data.text;
+  } catch (error) {
+    console.error('Error transcribing voice message:', error);
+    throw error; // Re-throw the error after logging
+  } finally {
+    if (filePath) {
+      await fsPromises.unlink(filePath).catch(err => console.error('Error cleaning up temp file:', err));
     }
-  });
-
-  fs.unlinkSync(filePath); // Delete the temp file after processing
-  return transcriptionResponse.data.text;
+  }
 }
 
 export default transcribeVoiceMessage;
